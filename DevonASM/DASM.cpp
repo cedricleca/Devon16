2b65559e78d3fb024606d0f6958a073b66c393ca
cedricleca <54026963+cedricleca@users.noreply.github.com>
2021-05-11 22:23:10 +0200

    DASM work

--- DevonASM/DASM.cpp
+++ DevonASM/DASM.cpp
@@ -15,24 +15,29 @@ int main(int argc, char * argv[])
 		if(argc > 1)
 		{
 			DevonASM::Assembler ASM = DevonASM::Assembler();
-			ASM.AssembleFile(argv[1]);
-
-			if(argc > 2)
+			if(!ASM.AssembleFile(argv[1]))
 			{
-				if(0 == strncmp(argv[2], "-dca:", 5))
+				std::cin.get();
+			}
+			else
+			{
+				if(argc > 2)
 				{
-					ASM.ExportROMFile(argv[2] + 5, 0x20000, 0x20000, 0x200);
-					std::cout << "Done exporting cartridge file.\n";
-				}
-				else if(0 == strncmp(argv[2], "-dro:", 5))
-				{
-					ASM.ExportROMFile(argv[2] + 5, 0x0, 0x10000);
-					std::cout << "Done exporting rom file.\n";
-				}
-				else
-				{
-					std::cout << "-dca:filename.das to export cartridge file\n";
-					std::cout << "-dro:filename.dro to export rom file\n";
+					if(strlen(argv[2]) && 0 == strncmp(argv[2], "-dca:", 5))
+					{
+						ASM.ExportROMFile(argv[2] + 5, 0x20000, 0x20000, 0x200);
+						std::cout << "Done exporting cartridge file.\n";
+					}
+					else if(strlen(argv[2]) && 0 == strncmp(argv[2], "-dro:", 5))
+					{
+						ASM.ExportROMFile(argv[2] + 5, 0x0, 0x10000);
+						std::cout << "Done exporting rom file.\n";
+					}
+					else
+					{
+						std::cout << "-dca:filename.das to export cartridge file\n";
+						std::cout << "-dro:filename.dro to export rom file\n";
+					}
 				}
 			}
 		}
